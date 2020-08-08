774b65416570a331afa07036b7005f394af11b43
wandan23 <43700481+wandan23@users.noreply.github.com>
2020-08-08 23:08:59 +0545

    Array Walk

---
+++ 1389B.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define M 1000000007
+#define ll long long
+#define pb push_back
+#define fo(i,N) for(int i = 0 ; i < N ; i++)
+#define foo(i,x,N) for (int i = x; i < N ; i++)
+#define fill(a,val) memset(a,val,sizeof(a))
+#define fastio()  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+#define ff first
+#define ss second
+#define MAX 1234567
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(a) a.begin(),a.end()
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define inf 1e18
+#define vi vector<int>
+#define pii pair<int,int>
+#define pqb priority_queue<pair<pair<ll,ll>,int>>
+#define pqs priority_queue<ll,vector <ll>,greater<ll>>
+/// find_by_order() -- returns the iterator to the kth largest
+/// order_of_key() -- number of elements strictly less than k
+ll mpow(ll x, ll y, ll m) {if ( y == 0) return 1;  if ( y & 1) return (x % m * mpow(x, y - 1, m) % m) % m; else  {ll temp = mpow(x, y / 2, m) % m; return (temp * temp) % m;}}
+ll power(ll x, ll y) {if ( y == 0) return 1;  if ( y & 1) return (x * power(x, y - 1)); else  {ll temp = power(x, y / 2); return (temp * temp);}}
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        pbds;
+void wandan23() {
+    fastio()
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+    wandan23();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n, k, z;
+        cin >> n >> k >> z;
+        int arr[n + 1];
+        int pre[n + 1] = {0};
+        foo(i, 1, n + 1)
+        {
+            cin >> arr[i];
+            pre[i] = pre[i - 1] + arr[i];
+        }
+        int ans = pre[k];
+
+        for (int i = 1; i <= k + 1; i++)
+        {
+            int cur = pre[i];
+            int temp = k - i + 1;
+            int p = 0;
+            int x = 1;
+            while ( temp > 0 && p < z)
+            {
+                cur = cur + arr[i + x];
+                if ( x == 0)
+                    p++;
+                x  = 1 - x;
+                temp--;
+            }
+            if ( x == 0)
+            {
+                cur = cur + pre[i + 1 + temp] - pre[i + 1];
+            }
+            else
+                cur = cur + pre[i + temp] - pre[i];
+            ans = max(ans, cur);
+
+        }
+        cout << ans << endl;
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
